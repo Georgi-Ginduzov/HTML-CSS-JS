c9a996adf5093edd6cca99b15f996f9b322c5506
Georgi <123997331+bunnyhype@users.noreply.github.com>
2024-01-22 23:52:54 +0200

    fetch current weather data

---
+++ BDZ/js/weather.js
@@ -0,0 +1,23 @@
+let form=document.querySelector(`#train-submission-form`);
+
+form.addEventListener('submit', function(event) {
+    event.preventDefault();
+    let city = document.querySelector(`#depart-from-city`).value;
+    console.log(city);
+    window.location.href = `/plan-journey.html?city=${city}`;
+});
+
+let temp = document.querySelector('#temp');
+let urlParams = new URLSearchParams(window.location.search);
+let city = urlParams.get('city');
+console.log(city);
+
+const displayData =(weather)=>{
+    let celsius = weather.current.temp_c;
+    temp.innerText = `${celsius}°C`;
+}
+    
+fetch(`http://api.weatherapi.com/v1/current.json?key=ace1109ee27d4f01a45185757242201&q=${city}`)
+.then(response => response.json())
+.then(displayData)
+.catch(err => alert(`Wrong ${city} name!`));
