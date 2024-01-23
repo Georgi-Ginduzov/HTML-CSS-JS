5ef37c8526606b7aacab62bc697ace8fab7df276
Georgi <123997331+bunnyhype@users.noreply.github.com>
2024-01-23 15:25:06 +0200

    Fetch and display weather data based on user input

--- BDZ/js/weather.js
+++ BDZ/js/weather.js
@@ -1,23 +1,36 @@
-let form=document.querySelector(`#train-submission-form`);
+document.addEventListener('DOMContentLoaded', (event) =>{
+    let form=document.querySelector(`#train-submission-form`);  
+    let temp = document.querySelector('#temp'); 
 
-form.addEventListener('submit', function(event) {
-    event.preventDefault();
-    let city = document.querySelector(`#depart-from-city`).value;
-    console.log(city);
-    window.location.href = `/plan-journey.html?city=${city}`;
+    // Function to fetch and display weather data
+    const fetchWeather = (city) => {
+        fetch(`http://api.weatherapi.com/v1/current.json?key=ace1109ee27d4f01a45185757242201&q=${city}`)
+        .then(response => response.json())
+        .then(weather => {
+            let celsius = weather.current.temp_c;
+            temp.innerText = `${celsius}°C`;
+            let iconURL = weather.current.condition.icon;
+            if (!iconURL.startsWith('https:')) {
+                iconURL = 'https:' + iconURL;
+            }
+            let img = document.createElement('img');
+            img.src = iconURL;
+            temp.appendChild(img);
+        })
+        .catch(err => alert(`Wrong city name!`));
+    };
+
+    // Fetch weather data when the page loads
+    let urlParams = new URLSearchParams(window.location.search);
+    let city = urlParams.get('city');
+    if (city) {
+        fetchWeather(city);
+    }
+
+    // Fetch weather data when a city name is submitted in the form
+    form.addEventListener('submit', function(event) {
+        event.preventDefault();
+        city = document.querySelector(`#depart-from-city`).value;
+        fetchWeather(city);
+    });
 });
-
-let temp = document.querySelector('#temp');
-let urlParams = new URLSearchParams(window.location.search);
-let city = urlParams.get('city');
-console.log(city);
-
-const displayData =(weather)=>{
-    let celsius = weather.current.temp_c;
-    temp.innerText = `${celsius}°C`;
-}
-    
-fetch(`http://api.weatherapi.com/v1/current.json?key=ace1109ee27d4f01a45185757242201&q=Sofia`)
-.then(response => response.json())
-.then(displayData)
-.catch(err => alert(`Wrong ${city} name!`));
