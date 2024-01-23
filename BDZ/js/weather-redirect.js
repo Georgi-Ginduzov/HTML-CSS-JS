38ddf086bd49c6ce58ef8a1d04dc0245c0f6c62d
Georgi <123997331+bunnyhype@users.noreply.github.com>
2024-01-23 15:27:03 +0200

    Redirect to live trains page and send url data

--- BDZ/js/weather-redirect.js
+++ BDZ/js/weather-redirect.js
@@ -1,8 +1,7 @@
-let form=document.querySelector(`#train-submission-form`);
+let form = document.querySelector(`#train-submission-form`);
 
 form.addEventListener('submit', function(event) {
     event.preventDefault();
     let city = document.querySelector(`#depart-from-city`).value;
-    console.log(city);
-    window.location.href = `/plan-journey.html?city=${city}`;
+    window.location.href = `http://127.0.0.1:5501/plan-jorney.html?city=${city}`;
 });
